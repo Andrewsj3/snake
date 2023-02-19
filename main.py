b81a078aeda894323bedc4d8fc3576c8ba5e81b6
Andrewsj3 <andrewsj3@middleton.school.nz>
2023-02-20 07:40:05 +1300

    Added highscore and ability to play again

--- main.py
+++ main.py
@@ -30,18 +30,36 @@ class Snake:
         self.y = HEIGHT // 2
         self.dx = 20
         self.dy = 0
+        self.alive = True
+        self.score = 0
+        self.high_score = self.load_high_score()
         self.segments.append([self.x, self.y])
 
     def check_collision(self, apple):
-        if self.x < 0 or self.y < 0 or self.x > WIDTH or self.y > HEIGHT:
+        if self.x < 0 or self.y < 0 or self.x > WIDTH - 20 or self.y > HEIGHT - 20:
             # When the snake goes outside the boundary
-            exit()
+            self.alive = False
+            return True
         if [self.x, self.y] in self.segments[1:]:
             # When the snake's head hits its body
-            exit()
+            self.alive = False
+            return True
         if self.x == apple.x and self.y == apple.y:
             self.segments.append([self.x, self.y])
+            self.score += 1
             apple.spawn()
+
+    def load_high_score(self):
+        try:
+            with open("highscore.txt") as f:
+                score = int(f.read())
+                return score
+        except FileNotFoundError:
+            return 0
+
+    def save_high_score(self):
+        with open("highscore.txt", 'w') as f:
+            f.write(str(self.score))
 
     def update(self, head_x, head_y):
         # Called each frame to update the snake's position on screen
@@ -54,13 +72,15 @@ class Snake:
         self.update(self.x, self.y)
         # Updating where the snake should be drawn
         win.blit(snake_head, self.segments[0])
-        # Using different sprite for head so that the user 
+        # Using different sprite for head so that the user
         # can see which direction the snake is going
         for coords in self.segments[1:]:
             win.blit(snake_body, coords)
         # Drawing each segment of the body
         win.blit(apple_img, [apple.x, apple.y])
         # Drawing the apple
+        message(f"Score: {self.score}", 0xFFFFFF, (100, 50))
+        message(f"High Score: {self.high_score}", 0xFFFFFF, (600, 50))
         pygame.display.update()
         # Rendering our changes
 
@@ -81,6 +101,25 @@ class Apple:
             self.spawn()
 
 
+def hex_to_rgb(col: int):
+    from sys import byteorder
+    col_as_bytes = [(col >> (8 * 0)) & 0xFF,
+                    (col >> (8 * 1)) & 0xFF,
+                    (col >> (8 * 2)) & 0xFF]
+    if byteorder == "little":
+        # order needs to be reversed for little-endian machines
+        col_as_bytes = col_as_bytes[::-1]
+    return col_as_bytes
+
+
+def message(text, text_col, coords, bg_col=None):
+    text_col = hex_to_rgb(text_col)
+    bg_col = None and hex_to_rgb(bg_col)
+    txt = font.render(text, True, text_col, bg_col)
+    text_box = txt.get_rect(center=coords)
+    win.blit(txt, text_box)
+
+
 def main():
     snake = Snake()
     apple = Apple(snake)
@@ -89,6 +128,8 @@ def main():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                if snake.score > snake.high_score:
+                    snake.save_high_score()
                 pygame.quit()
                 exit()
             if event.type == pygame.KEYDOWN:
@@ -124,11 +165,28 @@ def main():
                 # Added break to prevent player from being
                 # able to change direction twice in one frame
                 break
-        snake.x += snake.dx
-        snake.y += snake.dy
-        snake.check_collision(apple)
-        snake.draw(apple)
-        clock.tick(5)
+        if snake.alive:
+            snake.x += snake.dx
+            snake.y += snake.dy
+            if snake.check_collision(apple):
+                break
+            snake.draw(apple)
+        else:
+            break
+        clock.tick(max(5, len(snake.segments)//2))
+    while True:
+        win.fill(0x000000)
+        message("You died! Press 'q' to quit or 'a' to play again",
+                0xFFFFFF, (WIDTH//2, HEIGHT//2))
+        pygame.display.update()
+        if snake.score > snake.high_score:
+            snake.save_high_score()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    exit()
+                elif event.key == pygame.K_a:
+                    main()
 
 
 if __name__ == "__main__":
